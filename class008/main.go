1e4ec3d3d508182926a810fa3b677ee659c45274
solo_zhang <ericzhangmeng@163.com>
2022-04-01 17:53:13 +0800

    create class 006

--- class008/main.go
+++ class008/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //指针和地址
 //指针
 //func main() {
@@ -14,23 +16,23 @@ package main
 //}
 
 // 数据指针和指针数组
-//func main() {
-//	var arr [5]string
-//	arr = [5]string{"1", "2", "3", "4", "5"}
-//	var arrP *[5]string
-//	arrP = &arr
-//	fmt.Println(arr, arrP)
-//
-//	var arrpp [5]*string
-//
-//	var str1 string = "str1"
-//	var str2 string = "str2"
-//	var str3 string = "str3"
-//	var str4 string = "str4"
-//	var str5 string = "str5"
-//
-//	arrpp = [5]*string{&str1, &str2, &str3, &str4, &str5}
-//	*arrpp[2] = "666"
-//	fmt.Println(str3)
-//	fmt.Println(*arrpp[0], *arrpp[1], *arrpp[2], *arrpp[3], *arrpp[4])
-//}
+func main() {
+	var arr [5]string
+	arr = [5]string{"1", "2", "3", "4", "5"}
+	var arrP *[5]string
+	arrP = &arr
+	fmt.Println(arr, arrP)
+
+	var arrpp [5]*string
+
+	var str1 string = "str1"
+	var str2 string = "str2"
+	var str3 string = "str3"
+	var str4 string = "str4"
+	var str5 string = "str5"
+
+	arrpp = [5]*string{&str1, &str2, &str3, &str4, &str5}
+	*arrpp[2] = "666"
+	fmt.Println(str3)
+	fmt.Println(*arrpp[0], *arrpp[1], *arrpp[2], *arrpp[3], *arrpp[4])
+}
